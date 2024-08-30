4fbd485c13ea20ed157e5d23117ce2e567f65179
Dan Adrian Mirea <damirea84@gmail.com>
2024-08-30 23:31:31 +0300

    remove init.bat

--- init.bat
+++
@@ -1,7 +0,0 @@
-echo "quake_ironwail" >> README.md
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:danadrianmirea/quake_ironwail.git
-git push -u origin main
